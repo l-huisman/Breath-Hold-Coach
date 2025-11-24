83b82de1a74815e885a20521a664d0203bbbe283
Luke Huisman <huisman.l@yahoo.com>
2025-11-24 14:35:53 +0100

    Refactor TabLayout to remove unused color scheme and add modal component

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,12 +4,8 @@ import React from 'react';
 import {CustomBottomTabBar} from '@/components/bottom-tab-navigator';
 import {HapticTab} from '@/components/haptic-tab';
 import {IconSymbol} from '@/components/ui/icon-symbol';
-import {Colors} from '@/constants/theme';
-import {useColorScheme} from '@/hooks/use-color-scheme';
 
 export default function TabLayout() {
-    const colorScheme = useColorScheme();
-
     return (
         <Tabs
             tabBar={(props) => <CustomBottomTabBar {...props} />}
